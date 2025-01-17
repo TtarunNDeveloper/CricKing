8abbdd1cc0edf8a380b48704a67fb2d3d3bd338e
Tarun <2217115tarun@staloysius.ac.in>
2025-01-17 14:13:47 +0530

    Commit at 2.13pm

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,12 @@
-//import axios from 'axios'; //uncomment for API Data
+import axios from 'axios'; //uncomment for API Data
 import { mockLiveScores } from './mockData';
 
-/*const API_URL = 'https://api.cricapi.com/v1/cricScore';
+const API_URL = 'https://api.cricapi.com/v1/cricScore';
 const API_KEY = 'dab0c726-25b4-46ee-9b65-74ecb2286e63';
-**/ //uncomment for API data
+ //uncomment for API data
 
 export const fetchLiveScores = async () => {
-  /*
+  
   try {
     const response = await axios.get(`${API_URL}?apikey=${API_KEY}`);
     if (response.data.status !== 'success') {
@@ -18,8 +18,8 @@ export const fetchLiveScores = async () => {
     console.error('Error fetching live scores, using mock data:', error);
     return { data: mockLiveScores.data, isMock: true };
   }
- **/ //uncomment for API data
+  //uncomment for API data
 
   // Using mock data for now
-  return { data: mockLiveScores.data, isMock: true };
+  //return { data: mockLiveScores.data, isMock: true };
 };
